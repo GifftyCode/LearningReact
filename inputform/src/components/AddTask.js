bcc7d6866b18b5dd9b3747da8d7d291a74adaf42
GifftyCode <ulokangozi@yahoo.com>
2024-06-26 01:38:54 +0100

    input forms and events

---
+++ inputform/src/components/AddTask.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import './AddTask.css';
+
+export const AddTask = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [progress, setProgress] = useState(false);
+
+  const handleReset = () => {
+    setInputValue('');
+    setProgress(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const task = {
+      id: Math.floor(Math.random() * 1000),
+      name: inputValue,
+      completed: Boolean(progress),
+    };
+    console.log(task);
+    handleReset();
+  };
+  return (
+    <section className='addtask'>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setInputValue(e.target.value)}
+          type='text'
+          name='task'
+          id='task'
+          placeholder='Enter a Task'
+          autoComplete='off'
+          value={inputValue}
+        />
+        <select onChange={(e) => setProgress(e.target.value)} value={progress}>
+          <option value='false'>Pending</option>
+          <option value='true'>Completed</option>
+        </select>
+        <button type='submit'>Add</button>
+        <span onClick={handleReset} className='reset'>
+          Reset
+        </span>
+      </form>
+      <p>{inputValue}</p>
+    </section>
+  );
+};
