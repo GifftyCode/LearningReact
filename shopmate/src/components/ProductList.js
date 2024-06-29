d99b4a04b8f31de0c8258f86b60e8ef8487c4937
GifftyCode <ulokangozi@yahoo.com>
2024-06-29 21:06:23 +0100

    using custom hook

--- shopmate/src/components/ProductList.js
+++ shopmate/src/components/ProductList.js
@@ -1,24 +1,33 @@
-import { useCallback, useEffect, useState } from 'react';
+// import { useCallback, useEffect, useState } from 'react';
+
+import { useState } from 'react';
+import useFetch from '../hooks/useFetch';
 
 export const ProductList = () => {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
   const [url, setUrl] = useState('http://localhost:8000/products');
 
+  const { data: products } = useFetch(url);
+
+  // Using a direct fxn on our useEffect to fetch our API
   // useEffect(() => {
   //   fetch(url)
   //     .then((response) => response.json())
   //     .then((data) => setProducts(data));
   // }, [url]);
 
-  const fetchProducts = useCallback(async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    setProducts(data);
-  }, [url]);
+  // Calling the fetch fxn defined in another page on outside our useEffect
+  // const fetchProducts = useCallback(async () => {
+  //   const response = await fetch(url);
+  //   const data = await response.json();
+  //   setProducts(data);
+  // }, [url]);
 
-  useEffect(() => {
-    fetchProducts();
-  }, [fetchProducts]);
+  // useEffect(() => {
+  //   fetchProducts();
+  // }, [fetchProducts]);
+
+  // using our custom hook to fetch any endpoint
 
   return (
     <section>
@@ -32,18 +41,19 @@ export const ProductList = () => {
           In Stock
         </button>
       </div>
-      {products.map((product) => (
-        <div className='card' key={product.id}>
-          <p className='id'>{product.id}</p>
-          <p className='name'>{product.name}</p>
-          <p className='info'>
-            <span>${product.price}</span>
-            <span className={product.in_stock ? 'instock' : 'unavailable'}>
-              {product.in_stock ? 'In stock' : 'Unavailable'}
-            </span>
-          </p>
-        </div>
-      ))}
+      {products &&
+        products.map((product) => (
+          <div className='card' key={product.id}>
+            <p className='id'>{product.id}</p>
+            <p className='name'>{product.name}</p>
+            <p className='info'>
+              <span>${product.price}</span>
+              <span className={product.in_stock ? 'instock' : 'unavailable'}>
+                {product.in_stock ? 'In stock' : 'Unavailable'}
+              </span>
+            </p>
+          </div>
+        ))}
     </section>
   );
 };
