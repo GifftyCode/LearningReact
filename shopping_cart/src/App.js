524ceed1d44e2b647017c01873d7e682f9df9404
GifftyCode <ulokangozi@yahoo.com>
2024-07-18 00:37:46 +0100

    header file

---
+++ shopping_cart/src/App.js
@@ -0,0 +1,17 @@
+import { Header, Footer } from './components';
+
+import './App.css';
+
+import { AllRoutes } from './routes/AllRoutes';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <AllRoutes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
