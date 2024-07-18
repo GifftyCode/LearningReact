0017ae2f299c1c9c8e23f45c8eabf20a9ccaebd1
GifftyCode <ulokangozi@yahoo.com>
2024-07-18 22:00:59 +0100

    cartPage

--- shopping_cart/src/pages/Cart.js
+++ shopping_cart/src/pages/Cart.js
@@ -1,3 +1,29 @@
+import { CartCard } from '../components';
+
 export const Cart = () => {
-  return <div></div>;
+  const products = [
+    {
+      id: 1,
+      name: 'ZenithX Hi-Fi 360 Headphones',
+      price: 149,
+      image: '/assets/images/img1.jpg',
+    },
+    {
+      id: 2,
+      name: 'EchoPods BT200 Bluetooth Earphones',
+      price: 49,
+      image: '/assets/images/img2.jpg',
+    },
+  ];
+
+  return (
+    <main>
+      <section className='cart'>
+        <h1>Cart Items: {products.length}</h1>
+        {products.map((product) => (
+          <CartCard key={product.id} product={product} />
+        ))}
+      </section>
+    </main>
+  );
 };
