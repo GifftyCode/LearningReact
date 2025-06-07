f6b6e0daa6107b462c3f2c816b76d5f5b1df20c9
GifftyCode <ulokangozi@yahoo.com>
2025-06-07 17:59:11 +0100

    useEffect hook

--- ToBuyList/src/App.jsx
+++ ToBuyList/src/App.jsx
@@ -4,37 +4,52 @@ import Footer from "./Footer";
 import Header from "./Header";
 import AddItem from "./AddItem";
 import SearchItem from "./SearchItem";
+import { useEffect } from "react";
 
 function App() {
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("shoppingList"))
-  );
+  const API_URL = "http://localhost:4000/items";
 
+  const [items, setItems] = useState([]);
   const [newItems, setNewItems] = useState("");
   const [search, setSearch] = useState("");
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const handleSetAndSave = (newItem) => {
-    setItems(newItem);
-    localStorage.setItem("shoppingList", JSON.stringify(newItem));
-  };
+  useEffect(() => {
+    const fectchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error("Did not receive valid data");
+        const listItems = await response.json();
+        setItems(listItems);
+        setFetchError(null);
+      } catch (error) {
+        setFetchError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fectchItems();
+  }, []);
 
   const handleAdd = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItems = { id, checked: false, item };
     const listItems = [...items, myNewItems];
-    handleSetAndSave(listItems);
+    setItems(listItems);
   };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    handleSetAndSave(listItems);
+    setItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    handleSetAndSave(listItems);
+    setItems(listItems);
   };
 
   const handleSubmit = (e) => {
@@ -52,13 +67,19 @@ function App() {
         handleSubmit={handleSubmit}
       />
       <SearchItem search={search} setSearch={setSearch} />
-      <Content
-        items={items.filter((item) =>
-          item.item.toLowerCase().includes(search.toLowerCase())
+      <main className="flex-1 p-6 max-w-3xl mx-auto w-full">
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && (
+          <Content
+            items={items.filter((item) =>
+              item.item.toLowerCase().includes(search.toLowerCase())
+            )}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
         )}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+      </main>
       <Footer length={items.length} />
     </>
   );
--- ToBuyList/src/Content.jsx
+++ ToBuyList/src/Content.jsx
@@ -2,7 +2,7 @@ import ListItems from "./ListItems";
 
 const Content = ({ items, handleCheck, handleDelete }) => {
   return (
-    <main className="flex-1 p-6 max-w-3xl mx-auto w-full">
+    <>
       {items.length ? (
         <ListItems
           items={items}
@@ -12,7 +12,7 @@ const Content = ({ items, handleCheck, handleDelete }) => {
       ) : (
         <p className="mt-6">List Is Empty!!!</p>
       )}
-    </main>
+    </>
   );
 };
 
