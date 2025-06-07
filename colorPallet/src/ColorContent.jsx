08a90a935c73e507fe6420045e47f591135132ca
GifftyCode <ulokangozi@yahoo.com>
2025-06-07 13:08:14 +0100

    toggler

--- colorPallet/src/ColorContent.jsx
+++ colorPallet/src/ColorContent.jsx
@@ -4,7 +4,7 @@ const ColorContent = ({ colorValue, hexValue, isDarkText }) => {
   return (
     <div
       className="w-full max-w-2xl h-64 mt-6 border-2 border-gray-300 rounded-lg
-      flex items-center justify-center text-gray-500 text-lg"
+      flex items-center justify-center text-gray-500 text-lg "
       style={{
         backgroundColor: colorValue || "transparent",
         color: isDarkText ? "#fff" : "#000",
